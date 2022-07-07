22265df271f0a88b13fc171f0034f3b01f5fbc3f
Alan Leonel Perez <004862867@coyote.csusb.edu>
2022-07-07 04:40:47 -0700

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    public float speed;
+    public float randomUp;
+
+    Rigidbody2D ballRigidbody;
+    GameController cont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidbody = GetComponent<Rigidbody2D>();
+        cont = FindObjectOfType<GameController>();
+    }
+
+    private void OnEnable()
+    {
+        Invoke("PushBall", 1f);
+    }
+
+    void PushBall()
+    {
+
+        int dir = Random.Range(0, 2);// random 0, 1
+        float x, y;
+        if (dir == 0) // move to right
+            x = speed;
+        else // move to left
+            x = -speed;
+
+        y = Random.Range(-randomUp, randomUp);
+        ballRigidbody.AddForce(new Vector2(x, y));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OncollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+
+            Vector2 vel;
+            vel.x = ballRigidbody.velocity.x;
+            vel.y = ballRigidbody.velocity.y/2 + collision.collider.attachedRigidbody.velocity.y / 2;
+            ballRigidbody.velocity = vel;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+
+            if (ballRigidbody.velocity.x > 0) //collider right wall, left player gets score 
+            {
+                cont.Score(true);
+            }
+            else if(ballRigidbody.velocity.x < 0)// left wall
+            {
+                cont.Score(false);
+            }
+            else { }
+
+            ballRigidbody.velocity = Vector2.zero;// new  Vector2(0, 0)
+            transform.position = Vector3.zero; 
+
+            Invoke("PushBall", 2f);
+        }
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public bool leftPlayer;
+    public float speed;
+
+    int leftUp, rightUp;
+ 
+    Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftPlayer) // control left paddle
+        {
+            if (Input.GetKey(KeyCode.W))
+                leftUp = 1;
+            if (Input.GetKey(KeyCode.S))
+                leftUp = -1;
+
+            rigidbody.AddForce(Vector2.up * leftUp * speed * Time.deltaTime);
+            // new Vector2(0, leftUp * speed * Time.deltaTime)
+        }
+        else // control right paddle 
+        { 
+            if (Input.GetKey(KeyCode.UpArrow))
+                rightUp = 1;
+            if (Input.GetKey(KeyCode.DownArrow))
+                rightUp = -1;
+
+            rigidbody.AddForce(Vector2.up * rightUp * speed * Time.deltaTime); 
+        }
+    }
+}
